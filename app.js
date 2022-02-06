dd9a7daf2c0be720611f33a67df2b618f267af3c
Nikunj210903-Maruti-Internship <nikunj.vadodariya@marutitech.com>
2022-02-06 10:29:10 +0530

    removing node_modules

--- app.js
+++ app.js
@@ -7,4 +7,4 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
-app.listen(process.env.PORT);
+app.listen(process.env.PORT || 5000);
